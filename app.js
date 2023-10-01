e18395f743fd27596fea8ead12431d5ffb829da4
vivvk <vickykumarsunilja@gmail.com>
2023-10-01 15:40:15 +0530

    Fixing Proxy

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const compression = require('compression');
 const bookingController = require('./controllers/bookingController');
 
 const app = express();
-app.enable('trust proxy');
+app.set('trust proxy', 1);
+app.get('/x-forwarded-for', (request, response) =>
+  response.send(request.headers['x-forwarded-for']),
+);
 
 app.use(express.static(`${__dirname}/public`)); //this will render public folder html
 //Setting up pug template
